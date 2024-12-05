b67adce5351a7ee06e2f5db524fd255bb711bc9b
Paul Osborne <osbpau@gmail.com>
2024-12-05 02:17:13 -0600

    Use flake rather than devenv for dependencies

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        my-rust = pkgs.rust-bin.stable.latest.complete;
+      in
+      {
+        devShell = with pkgs; mkShell {
+          buildInputs = [
+            bacon
+            cargo-nextest
+            my-rust
+          ];
+          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          CARGO_TARGET_WASM32_UNKNOWN_UNKNOWN_LINKER = "wasm-ld";
+        };
+      }
+    );
+}
