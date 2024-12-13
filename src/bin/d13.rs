1f49e2b864115d23dc28eaab6b3cb06ff5e117d1
Paul Osborne <osbpau@gmail.com>
2024-12-13 01:42:12 -0600

    Notes to pick up on tomorrow, sleepy time

--- src/bin/d13.rs
+++ src/bin/d13.rs
@@ -84,30 +84,67 @@ fn parse_input<P: AsRef<Path>>(path: P) -> anyhow::Result<Vec<ClawMachine>> {
 
 fn find_optimal_naive(machine: &ClawMachine) -> Option<(usize, usize)> {
     let mut working: HashSet<(usize, usize)> = HashSet::new();
-    for a_hits in 0..100 {
-        for b_hits in 0..100 {
+    let mut a_hits = 0;
+    while a_hits * machine.a.x <= machine.prize_location.0
+        && a_hits * machine.a.y <= machine.prize_location.1
+    {
+        let mut b_hits = 0;
+        loop {
             let position = (
                 machine.a.x * a_hits + machine.b.x * b_hits,
                 machine.a.y * a_hits + machine.b.y * b_hits,
             );
             // TODO: could exit earlier here with cmps
-            if position == machine.prize_location {
+            if position.0 > machine.prize_location.0 || position.1 > machine.prize_location.1 {
+                break; // dead end
+            } else if position == machine.prize_location {
                 working.insert((a_hits, b_hits));
             }
+            b_hits += 1;
         }
+        a_hits += 1;
     }
 
     working.into_iter().min_by_key(|(a, b)| a * 3 + b)
 }
 
 fn main() -> anyhow::Result<()> {
-    let machines = parse_input("d13.txt")?;
+    let mut machines = parse_input("d13-example1.txt")?;
     let mut tokens = 0;
-    for machine in machines {
-        if let Some((a, b)) = find_optimal_naive(&machine) {
+    for machine in machines.iter() {
+        if let Some((a, b)) = find_optimal_naive(machine) {
             tokens += a * 3 + b;
         }
     }
-    println!("Tokens: {tokens:?}");
+    println!("Part 1 Tokens: {tokens:?}");
+
+    // now add 10000000000000 to X/Y of the inputs for part 2
+    //
+    // Doing this brute force is too compuationally expensive as expected;
+    // for tomorrow, probably look to see what we can do with optimizing
+    // a system of equations based on the mathematical system of equations
+    // we get for a/b hits, p_x/p_y (prize_loc), and m_x,m_y (machine x/y cal.)
+    //
+    // On paper I think we have the following, though my math is rusty.
+    // - a = px/mx - b
+    // - a = py/my - b
+    //
+    // As a constraint, we only care about the set of all positive integers;
+    // if we can find all solutions somehow, we can probably stil get away
+    // with finding the optimal solution (based on cost) in a naive fashion.
+    //
+    // Probably combine this into a simpler system of equations.
+    for machine in machines.iter_mut() {
+        machine.prize_location.0 += 10000000000000;
+        machine.prize_location.1 += 10000000000000;
+    }
+    tokens = 0;
+    for machine in machines.iter() {
+        if let Some((a, b)) = find_optimal_naive(machine) {
+            tokens += a * 3 + b;
+        }
+    }
+    println!("Part 2 Tokens: {tokens:?}");
+
     Ok(())
 }
