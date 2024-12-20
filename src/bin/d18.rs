13b26e7ffe58e2d5c2ced328b6a5946690659930
Paul Osborne <osbpau@gmail.com>
2024-12-20 17:15:53 -0600

    Day 18: Getting closer to having A* working (I think)
    We compute the right solution for the small input puzzle
    but things are running for a long time on the full
    input set, so I think something is off with the impl -- I need
    to get a better idea of exactly how I should be implementing
    Ord here (though that may not be the issue for non-halting).

--- src/bin/d18.rs
+++ src/bin/d18.rs
@@ -1,11 +1,12 @@
 use std::{
-    collections::{BinaryHeap, HashSet},
+    collections::{BinaryHeap, HashSet, VecDeque},
     fmt::Display,
     path::Path,
 };
 
 use aoc::input_lines;
 use clap::Parser;
+use colored::Colorize;
 
 #[derive(Debug, Copy, Clone)]
 enum MapEntry {
@@ -73,16 +74,20 @@ const DELTAS: [(isize, isize); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 struct Node {
-    // x/y
+    /// x/y
     position: Position,
-    // cost to move to this position from the start
+
+    /// cost to move to this position from the start
     cost: usize,
-    // estimated cost from this position to the goal
-    estimated_remaining_cost: usize,
-    // estimate total cost (if computed)
+
+    /// estimated cost from this position to the goal
+    estimated_cost_to_goal: usize,
+
+    /// Estimated cost from start to goal along this point
     estimated_total_cost: usize,
-    // previous node position
-    prev: Option<Position>,
+
+    /// previous node position to here
+    prev: Option<Box<Node>>,
 }
 
 impl Default for Node {
@@ -91,15 +96,15 @@ impl Default for Node {
             estimated_total_cost: usize::MAX,
             position: Default::default(),
             cost: 0,
-            estimated_remaining_cost: 0,
+            estimated_cost_to_goal: usize::MAX,
+            prev: None,
         }
     }
 }
 
 impl Ord for Node {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.cost
-            .cmp(&other.cost)
+        self.estimated_cost_to_goal.cmp(&other.estimated_cost_to_goal)
             .then(self.position.cmp(&other.position))
     }
 }
@@ -110,91 +115,80 @@ impl PartialOrd for Node {
     }
 }
 
-fn init_2darray<T: Default>(cols: usize, rows: usize) -> Vec<Vec<T>> {
-    (0..cols)
-        .map(|_| (0..rows).map(|_| Default::default()).collect())
-        .collect()
-}
-
-fn solve_maze_using_astar(map: &[Vec<MapEntry>]) -> Option<Node> {
-    let mut open_set = BinaryHeap::new();
-    let start_node = Node::default();
-    open_set.push(start_node);
-    let goal = Position {
-        x: map.len() - 1,
-        y: map.len() - 1,
-    };
-
-    let mut nodes: Vec<Vec<Node>> = (0..map.len())
-        .map(|y| {
-            (0..map.len())
-                .map(|x| Node {
-                    position: Position { x, y },
-                    ..Default::default()
-                })
-                .collect()
-        })
-        .collect();
-    let mut visited = init_2darray::<bool>(map.len(), map.len());
-
-    while let Some(Node {
-        position: Position { x, y },
-        cost,
-        ..
-    }) = open_set.pop()
-    {
-        println!("Visiting y={y}, x={x}");
-        visited[y][x] = true;
-
-        let successors = DELTAS.iter().filter_map(|(dx, dy)| {
-            let nx = x.checked_add_signed(*dx)?;
-            let ny = y.checked_add_signed(*dy)?;
+fn find_neighbors(map: &[Vec<MapEntry>], position: Position) -> Vec<Position> {
+    DELTAS
+        .iter()
+        .filter_map(move |(dx, dy)| {
+            let nx = position.x.checked_add_signed(*dx)?;
+            let ny = position.y.checked_add_signed(*dy)?;
             if nx >= map.len() || ny >= map.len() {
                 return None;
             }
             if !matches!(map[ny][nx], MapEntry::Open) {
                 None
             } else {
-                Some((nx, ny))
+                Some(Position { x: nx, y: ny })
             }
-        });
+        })
+        .collect()
+}
 
-        for (nx, ny) in successors {
-            println!("ny={ny}, nx={nx}");
-            let node = &mut nodes[ny][nx];
+fn solve_maze_using_astar(map: &[Vec<MapEntry>]) -> Option<VecDeque<Position>> {
+    let mut frontier = BinaryHeap::new();
+    let start_node = Node::default();
+    frontier.push(start_node);
+    let goal = Position {
+        x: map.len() - 1,
+        y: map.len() - 1,
+    };
+    let mut visited: HashSet<Position> = HashSet::new();
 
-            if (nx, ny) == (goal.x, goal.y) {
-                node.cost = cost + 1;
-                return Some(node.clone());
+    while let Some(node) = frontier.pop() {
+        let Position { x, y } = node.position;
+        visited.insert(node.position);
+
+        // Are we at the goal?
+        if (x, y) == (goal.x, goal.y) {
+            // compute the path
+            let mut path = VecDeque::new();
+            let mut next_node = Some(Box::new(node.clone()));
+            while let Some(cur_node) = next_node {
+                path.push_back(cur_node.position);
+                next_node = cur_node.prev;
+            }
+            return Some(path);
+        }
+
+        for neigh_position in find_neighbors(map, node.position) {
+            if visited.contains(&neigh_position) {
+                continue;
             }
 
-            // E.g. "g"
-            let ncost = 1 + cost;
+            let cost = node.cost + 1;
+            let estimated_cost_to_goal = (goal.y - y) + (goal.x - x); //  manhattan
+            let estimated_total_cost = estimated_cost_to_goal + cost;
 
-            // Since we can only move in 4 directions, we want to use
-            // the manhattan distance here which is just the sum
-            // of the differences between points and goal on our grid.
-            //
-            // E.g. "h"
-            let estimated_cost_to_goal = {
-                // we know goal is >= nx/ny so abs not required.
-                (goal.y - ny) + (goal.x - nx)
+            let prev = Box::new(node.clone());
+            let neigh_node = Node {
+                position: neigh_position,
+                cost,
+                estimated_cost_to_goal,
+                estimated_total_cost,
+                prev: Some(prev),
             };
 
-            // e.g. "f"
-            let estimated_total_cost = ncost + estimated_cost_to_goal;
-
-            if node.estimated_total_cost == usize::MAX
-                || estimated_total_cost > node.estimated_total_cost && !visited[ny][nx]
+            if let Some(existing_in_frontier) = frontier
+                .iter()
+                .filter(|n| n.position == neigh_position)
+                .nth(0)
             {
-                node.estimated_total_cost = estimated_total_cost;
-                node.cost = ncost;
-                node.estimated_remaining_cost = estimated_cost_to_goal;
-                node.prev = Some(Position {
-                    x, y
-                });
-                open_set.push(node.clone());
+                if neigh_node.cost > existing_in_frontier.cost {
+                    continue; // skip this node withotu adding to frontier, dead end path to the node
+                }
             }
+
+            // add this node to the frontier in priority order (see Ord/PartialOrd)
+            frontier.push(neigh_node);
         }
     }
 
@@ -204,22 +198,34 @@ fn solve_maze_using_astar(map: &[Vec<MapEntry>]) -> Option<Node> {
 fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
     let corruption = parse_input(cli.input)?;
-    let mut map_with_corruption: Vec<Vec<MapEntry>> = (0..cli.dimensions)
+    let mut map: Vec<Vec<MapEntry>> = (0..cli.dimensions)
         .map(|_y| (0..cli.dimensions).map(|_x| MapEntry::Open).collect())
         .collect();
 
     for pos in corruption.iter().take(cli.bytes) {
-        map_with_corruption[pos.y][pos.x] = MapEntry::Corrupted;
+        map[pos.y][pos.x] = MapEntry::Corrupted;
     }
 
-    let mut node = solve_maze_using_astar(&map_with_corruption).expect("Expected Solution");
-    println!("Node: {:?}", node);
-    println!("Cost: {}", node.cost);
+    let path = solve_maze_using_astar(&map).expect("Expected Solution");
+    println!("Path: {:?}", path);
 
-    let mut positions: HashSet<Position> = HashSet::new();
-    while let Some(next) = node.prev {
-        positions.insert(next);
+    for y in 0..cli.dimensions {
+        for x in 0..cli.dimensions {
+            let entry = map[y][x];
+            let pos = Position { x, y };
+            let in_path = path.contains(&pos);
+            let s = match (in_path, entry) {
+                (true, _) => format!("{}", 'O').blue(),
+                (false, MapEntry::Open) => ".".into(),
+                (false, MapEntry::Corrupted) => "x".into(),
+            };
+            print!("{s}");
+        }
+        println!("");
     }
+
+    // The cost is the path length - 1 (the # of moves)
+    println!("Cost: {}", path.len() - 1);
 
     Ok(())
 }
